1ce1dd0fb610eaf0dc9c6647f4997911a8709db0
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-06 06:34:34 +0000

    listen on 80

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
