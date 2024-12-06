3edd0879f69be3ec00f658e388f9c97eb6a52eca
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-06 07:44:37 +0000

    try to move in the root

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(80, "0.0.0.0", () => {
+app.listen(8080, "0.0.0.0", () => {
   console.log("Service active on Seenode ✔️");
 });
