6ea1c8811d3aed7134af2167047032666143512a
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2024-12-06 08:22:07 +0000

    80

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080, '0.0.0.0');
+  await app.listen(80, '0.0.0.0');
   console.log('Application is running on http://0.0.0.0:8080');
 }
 bootstrap();
